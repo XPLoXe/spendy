8c29d9fc5b14a1f11ef5287699c4a9698056c0e8
Alejandro Bocchicchio Mesa <aleb2800@gmail.com>
2025-10-18 17:21:06 +0200

    Prepare for Vercel Deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: { preset: 'vercel' },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
